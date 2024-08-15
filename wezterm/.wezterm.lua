1b039f0a04bcd01cd59a81d0f215058f123233d1
Matt Lins <mattlins@gmail.com>
2024-08-15 17:13:12 -0500

    Switch font to CaskaydiaCove

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Regular" })
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
 config.font_size = 13
 config.line_height = 1.2
 
